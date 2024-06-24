08700bffb9f0831f1f0c41ae9c107994f5a5add0
Viorie Gerrid <57219804+vg2207@users.noreply.github.com>
2024-06-24 17:12:18 +0700

    Update auto_arrange_folder.py

--- auto_arrange_folder.py
+++ auto_arrange_folder.py
@@ -73,6 +73,7 @@ if user_input_excel is not None:
                 # lst.append(os.path.splitext(x)[0][-36 :])
 
             st.write(lst)
+            st.write(len(lst))
             if len(lst) != len(df) :
                 st.error('Data length not matched!')
 
